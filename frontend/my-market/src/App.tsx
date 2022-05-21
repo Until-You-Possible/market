5a3430438edcd97817e6fbe7f55b959400720224
ray <gang.wang05@sap.com>
2022-05-21 23:48:27 +0800

    1: 新建react+ts项目 2: 整理目录文件结构

---
+++ frontend/my-market/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './css/App.css';
+import Home from "./view/home";
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+
+  );
+}
+
+export default App;
---
+++ frontend/my-market/src/view/home/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+    return (
+        <div>
+            this is home
+        </div>
+    )
+}
+
+export default Home
