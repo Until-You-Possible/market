de32cdbb927092e7f8667ce46175c53bc9eeb758
ray <gang.wang05@sap.com>
2022-06-05 23:13:24 +0800

    1: 更新一些基本函数

--- frontend/my-market/src/util/helper.ts
+++ frontend/my-market/src/util/helper.ts
@@ -2,12 +2,9 @@
 
 import { Constants } from "../model/constant";
 import { message } from "antd";
-import { NavigateFunction, useNavigate} from "react-router-dom";
 
 
 class Helper {
-
-    // private navigate: NavigateFunction = useNavigate();
 
     // 基本的提示信息
     // type message.success/error/warn/info/loading/
@@ -32,11 +29,6 @@ class Helper {
             default:
                 message.success(content, duration).then( r => r);
         }
-    }
-
-    // 整个项目因为各种情况需要返回首页去登陆，重定向等情况
-    public redirectLogin() {
-        // this.navigate("/login");
     }
 }
 
---
+++ frontend/typescriptDoc/index.md
@@ -0,0 +1,5 @@
+### some question for typescript that make me confuse sometimes.
+
+
+1： what is different between "type" and "interface" ?
+
