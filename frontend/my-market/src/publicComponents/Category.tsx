98249876ae70b70c3ffeb53b812b3a41bcf8f289
ray <gang.wang05@sap.com>
2022-06-12 01:21:26 +0800

    1: 优化category的方法

--- frontend/my-market/src/publicComponents/Category.tsx
+++ frontend/my-market/src/publicComponents/Category.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import { Button, Carousel, Col, Row, Space } from "antd";
+import {Button, Carousel, Col, Row, Space} from "antd";
 import "../css/category.css";
 import slideList from "../mockData/sliders.json";
 import imgURL1 from "../img/banner/banner1.jpg";
@@ -8,15 +8,20 @@ import imgURL3 from "../img/banner/banner3.jpg";
 import imgURL4 from "../img/banner/banner4.jpg";
 import imgURL5 from "../img/banner/banner5.jpg";
 
-
+import categoriesList from "../mockData/category.json";
 
 const Category: React.FC = () => {
 
         const [size] = useState<number>(2);
 
+
         useEffect( () => {
-            console.log("imgURL1", imgURL1)
+
         }, []);
+
+        const onCategory = (event: any, keyword: any) => {
+            console.log("keyword", keyword);
+        }
 
         const judgeSlideImage = (index: number) => {
             if (index === 1) {
@@ -40,72 +45,23 @@ const Category: React.FC = () => {
             <Row gutter={[18,0]} className="categoryRow">
                 <Col span={6} className="goodWrapper">
                     <Row>
-                        <Col span={24}>
-                            <Space size={size}>
-                                <Button type="link">手机</Button>
-                                <Button type="link">数码</Button>
-                            </Space>
-                        </Col>
-                        <Col span={24}>
-                            <Button type="link">电脑</Button>
-                            <Button type="link">办公配件</Button>
-                        </Col>
-                        <Col span={24}>
-                            <Space size={size}>
-                                <Button type="link">电视</Button>
-                                <Button type="link">空调</Button>
-                                <Button type="link">冰箱</Button>
-                                <Button type="link">洗衣机</Button>
-                            </Space>
-                        </Col>
-                        <Col span={24}>
-                            <Space size={size}>
-                                <Button type="link">厨卫家电</Button>
-                                <Button type="link">小家电</Button>
-                            </Space>
-                        </Col>
-                        <Col span={24}>
-                            <Space size={size}>
-                                <Button type="link">家具</Button>
-                                <Button type="link">家装</Button>
-                            </Space>
-                        </Col>
-                        <Col span={24}>
-                            <Space size={size}>
-                                <Button type="link">个护化妆</Button>
-                                <Button type="link">清洁</Button>
-                                <Button type="link">纸品</Button>
-                            </Space>
-                        </Col>
-                        <Col span={24}>
-                            <Space size={size}>
-                                <Button type="link">母婴</Button>
-                                <Button type="link">玩具</Button>
-                                <Button type="link">童装童鞋</Button>
-                            </Space>
-                        </Col>
-                        <Col span={24}>
-                            <Space size={size}>
-                                <Button type="link">鞋靴</Button>
-                                <Button type="link">箱包</Button>
-                                <Button type="link">钟表</Button>
-                                <Button type="link">珠宝</Button>
-                            </Space>
-                        </Col>
-                        <Col span={24}>
-                            <Space size={size}>
-                                <Button type="link">户外运动</Button>
-                                <Button type="link">足球</Button>
-                                <Button type="link">汽车生活</Button>
-                            </Space>
-                        </Col>
-                        <Col span={24}>
-                            <Space size={size}>
-                                <Button type="link">图书</Button>
-                                <Button type="link">音响</Button>
-                                <Button type="link">电子书</Button>
-                            </Space>
-                        </Col>
+                        {
+                            categoriesList.categories.map((items, index) =>
+                                <Col span={24} key={index}>
+                                    <Space size={size}>
+                                        {
+                                            items.map((item, idx) =>
+                                                <Button key={idx}
+                                                        type="link"
+                                                        onClick={(event) => onCategory(event, item.text)}>
+                                                    {item.text}
+                                                </Button>
+                                            )
+                                        }
+                                    </Space>
+                                </Col>
+                            )
+                        }
                     </Row>
                 </Col>
                 <Col span={18}>
